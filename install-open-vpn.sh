324171a90aaf6f5f8f976218a644fa3b23b49d34
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2021-05-25 00:41:58 +0530

    Create install-open-vpn.sh

---
+++ install-open-vpn.sh
@@ -0,0 +1,10 @@
+
+
+ip addr show eth0
+ 
+apt update -y
+
+apt install -y liblzo2-2 bridge-utils net-tools python-pyrad python-serial libsasl2-2 iproute2 sqlite3 libsqlite3-0 iptables liblz4-1 python-pkg-resources python-mysqldb libmariadbclient18 libssl1.1
+
+dpkg -i openvpn-as-bundled-clients-16.deb openvpn-as_2.8.8-cbf850a0-Ubuntu18_amd64.deb
+
