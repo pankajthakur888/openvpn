d8cd43c694dec44bbf4e526983bb0cba53d3a6c0
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2021-05-26 00:02:48 +0530

    Update install-open-vpn-ubuntu-server20.04.sh

--- install-open-vpn-ubuntu-server20.04.sh
+++ install-open-vpn-ubuntu-server20.04.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-    apt update && apt upgrade -y
-    apt install -y libc6 libffi7 libgcc-s1 libgmp10 liblz4-1 liblzo2-2 libmariadb3 libpcap0.8 libssl1.1  libstdc++6 zlib1g bridge-utils net-tools libsasl2-2  iproute2  sqlite3  libsqlite3-0 iptables  python-pkg-resources python-netaddr wget
+    sudo apt update && apt upgrade -y
+    sudo apt install -y libc6 libffi7 libgcc-s1 libgmp10 liblz4-1 liblzo2-2 libmariadb3 libpcap0.8 libssl1.1  libstdc++6 zlib1g bridge-utils net-tools libsasl2-2  iproute2  sqlite3  libsqlite3-0 iptables  python-pkg-resources python-netaddr wget
     
         wget https://openvpn.net/downloads/openvpn-as-bundled-clients-latest.deb
     sudo dpkg -i openvpn-as-*.deb
